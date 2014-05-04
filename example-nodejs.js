924bee8645ba3123e6b77b8ad9c1c7431eb2c1b0
Changwan Jun <me@wani.kr>
2014-05-04 23:08:05 +0900

    example update

--- example-nodejs.js
+++ example-nodejs.js
@@ -7,6 +7,7 @@ fs = require('fs')
 fs.readFile('./README.md', 'utf-8', function(err, data) {
 
 	var contents = Tempy.read( data );
+
 	contents.assign('foo', 'This is foo string.');
 	contents.assign('bar', ['this is bar0','this is bar1','this is bar2','this is bar3']);
 	contents.assign('baz', [
@@ -29,7 +30,32 @@ fs.readFile('./README.md', 'utf-8', function(err, data) {
 	contents.assign('qux', [true, false, false, true]);
 
 	var rendered_contents = contents.render();
-
+	
+	/** 위 내용은 다음과 같이도 사용가능합니다.
+	var rendered_contents = contents.render({
+		foo : 'This is foo string.',
+		bar : ['this is bar0','this is bar1','this is bar2','this is bar3'],
+		baz : [
+			[
+				'this is baz0-0',
+				'this is baz0-1',
+			],
+			[
+				'this is baz1-0',
+				'this is baz1-1',
+				'this is baz1-2'
+			],
+			[
+				'this is baz2-0',
+				'this is baz2-1',
+				'this is baz2-2',
+				'this is baz2-3'
+			]
+		],
+		qux : [true, false, false, true]
+	})
+	// */
+	
 	console.log( rendered_contents );
 
 });
--- example-requirejs-main.js
+++ example-requirejs-main.js
@@ -33,6 +33,31 @@ require(['src/tempy.js'], function(tempy) {
 
 			var rendered_contents = contents.render();
 
+			/** 위 내용은 다음과 같이도 사용가능합니다.
+			var rendered_contents = contents.render({
+				foo : 'This is foo string.',
+				bar : ['this is bar0','this is bar1','this is bar2','this is bar3'],
+				baz : [
+					[
+						'this is baz0-0',
+						'this is baz0-1',
+					],
+					[
+						'this is baz1-0',
+						'this is baz1-1',
+						'this is baz1-2'
+					],
+					[
+						'this is baz2-0',
+						'this is baz2-1',
+						'this is baz2-2',
+						'this is baz2-3'
+					]
+				],
+				qux : [true, false, false, true]
+			})
+			// */
+			
 			document.getElementById('Example').innerHTML = rendered_contents;
 
 		}
